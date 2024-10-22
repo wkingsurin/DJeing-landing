5fce7a7b57a3bd30d5803d63d9f4d23b76ff7811
Daniil Surin <wkingsurin@gmail.com>
2024-10-22 15:42:07 +0500

    New music added

--- assets/utils.js
+++ assets/utils.js
@@ -1,1 +1,8 @@
-export const musicList = ['Nirvana-Smells-Like-Teen-Spirit.mp3', 'Nirvana-Heart-Shaped-Box.mp3', '', '', '', '', '']
+export const musicList = [
+    'Nirvana-Smells-Like-Teen-Spirit.mp3',
+    'Nirvana-Heart-Shaped-Box.mp3',
+    'Nirvana-Come-As-You-Are.mp3',
+    'Nirvana-Drain-You.mp3',
+    'Nirvana-In-Bloom.mp3',
+    'Nirvana-Something-In-The-Way.mp3',
+]
